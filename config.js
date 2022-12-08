953e94837a34c82670a56bb3a7a3ea698083a01a
treyeckel <38876839+treyeckel@users.noreply.github.com>
2022-12-08 11:04:37 -0600

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ const CONFIG = {
 	// Weather
 	weatherKey: '5ab0959a5ac299c30187d984821292fa', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'C', // 'F', 'C'
+	weatherUnit: 'F', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
